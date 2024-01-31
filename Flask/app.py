a406a44f1fa60d4553dc538f5ef30bdc274d56e3
andreeaa23 <dumitru_andreea2001@yahoo.com>
2024-01-31 20:47:54 +0200

    Added iptables gui

--- Flask/app.py
+++ Flask/app.py
@@ -1,28 +1,53 @@
-from flask import request, jsonify, Flask
+from flask import request, jsonify, Flask, send_from_directory
 from flask_cors import CORS
 import re
 import os
 import subprocess
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 
+#source retele/bin/activate =>activare mediu virtual
 
 # Get all iptables rules
-@app.route('/iptables', methods=['GET'])
-def get_iptables():
-    result = subprocess.run(['sudo', 'iptables', '-L', '-n', '-v'], capture_output=True, text=True)
+@app.route('/iptables/filter', methods=['GET'])
+def get_iptables_filter():
+    #result = subprocess.run(['sudo', 'iptables', '-t', 'filter', '-n', '-v' , '-L', 'INPUT', '--line-numbers'], capture_output=True, text=True)
+    result = subprocess.run(['sudo', 'iptables', '-t', 'filter', '-n', '-v' , '-L', '--line-numbers'], capture_output=True, text=True) # to see all filter rules
+
     return jsonify({'iptables_rules': result.stdout})
+
+@app.route('/iptables/nat', methods=['GET'])
+def get_iptables_nat():
+    #result = subprocess.run(['sudo', 'iptables', '-t', 'nat', '-n', '-v' , '-L',  'PREROUTING', '--line-numbers'], capture_output=True, text=True)
+    result = subprocess.run(['sudo', 'iptables', '-t', 'nat', '-n', '-v' , '-L',  '--line-numbers'], capture_output=True, text=True) #to see all nat rules
+
+    return jsonify({'iptables_rules': result.stdout})
+
 
 # Add a rule to iptables
 @app.route('/iptables/add', methods=['POST'])
 def add_iptables_rule():
     data = request.get_json()
-    rule = data.get('rule')
-    if not rule:
+    rule_table = data.get('rule_table')
+    rule_type = data.get('rule_type')
+    rule_action = data.get('rule_action')
+    ip_address = data.get('ip_address')
+    ip_address_destination = data.get('ip_address_destination')
+    ip_dest_final = ip_address_destination + ":80"
+    port = data.get('port')
+
+    if not rule_table or not rule_type or not rule_action or not ip_address or not port:
         return jsonify({'error': 'Invalid rule format'}), 400
 
-    subprocess.run(['sudo', 'iptables', '-A', rule])
+    if rule_table == "nat":
+        rule_command = ['sudo', 'iptables', '-t', rule_table, '-A', rule_type, '-p', 'tcp', '-d', ip_address, '--dport', port, '-j', rule_action, '--to', ip_dest_final]
+    else:
+        rule_command = ['sudo', 'iptables', '-A', rule_type, '-p', 'tcp', '-s', ip_address, '--dport', port, '-j', rule_action]
+        print("sudo iptables -A " + rule_type + " -p tcp -s " + ip_address + " --dport " + port + " -j " + rule_action)
+
+    subprocess.run(rule_command, capture_output=True, text=True)
+
     return jsonify({'message': 'Rule added successfully'})
 
 # Delete a rule from iptables
@@ -35,8 +60,14 @@ def delete_iptables_rule():
     if not rule_type or not rule_number:
         return jsonify({'error': 'Invalid rule format'}), 400
 
+    print("sudo iptables -D " +  rule_type + " " + rule_number)
     subprocess.run(['sudo', 'iptables', '-D', rule_type, rule_number])
-    return jsonify({'message': 'Rule deleted successfully'})
+    return jsonify({'message': 'Rule deleted successfully'})
+
+# Route to serve the HTML file
+@app.route('/')
+def serve_html():
+    return send_from_directory('static', 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
